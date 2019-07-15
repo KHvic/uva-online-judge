ecb06ac20804c1046dbb72000d9f881c16f12007
Goh Ka Hian <notesalt@hotmail.com>
2019-07-15 18:07:05 +0800

    frequency counting

---
+++ 10252-Common Permutation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a,b;
+    while(cin >> a){
+        cin >> b;
+        int aCount[26] = {};
+        int bCount[26] = {};
+        for(auto& c : a){
+            aCount[c-'a']++;
+        }
+        for(auto& c : b){
+            bCount[c-'a']++;
+        }
+        string res = "";
+        for(int i=0;i<26;i++){
+            int common = min(aCount[i],bCount[i]);
+            for(int j=0;j<common;j++)
+                res += (char)(i+'a');
+        }
+        cout << res << endl;
+    }
+}
